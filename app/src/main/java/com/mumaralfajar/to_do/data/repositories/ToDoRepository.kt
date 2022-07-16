fde3cb6e60e1f71b2bba881138a7606f0caa0d9d
mumaralfajar <kuhaaku@gmail.com>
2022-07-16 18:42:41 +0700

    create ToDoRepository

---
+++ app/src/main/java/com/mumaralfajar/to_do/data/repositories/ToDoRepository.kt
@@ -0,0 +1,39 @@
+package com.mumaralfajar.to_do.data.repositories
+
+import android.icu.text.StringSearch
+import com.mumaralfajar.to_do.data.ToDoDao
+import com.mumaralfajar.to_do.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    val getAllTask: Flow<List<ToDoTask>> = toDoDao.getAllTask()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDao.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDao.addTask(toDoTask = toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDao.updateTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDao.deleteTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteAllTask() {
+        toDoDao.deleteAllTask()
+    }
+
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>> {
+        return toDoDao.searchDatabase(searchQuery = searchQuery)
+    }
+
+}
